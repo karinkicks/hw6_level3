d3ec0e82befa705a817954707ee2ef27d4a596e4
karinkicks <KRot5601>
2020-12-12 00:36:39 +0300

    Initial commit

---
+++ src/main/java/ru/homework/ArrayUtils.java
@@ -0,0 +1,40 @@
+package ru.homework;
+
+public final class ArrayUtils {
+//
+//    private ArrayUtils(){}
+//
+//    public static int[] changeArray(int[] array){
+//        int temp = 0;
+//        boolean flag=true;
+//        for(int i=0;i<array.length;i++){
+//            if(array[i]==4){
+//                temp=i;
+//                flag=false;
+//            }
+//        }
+//        if(flag){
+//            throw new RuntimeException("SWW");
+//        }
+//        int[] newArray = new int[array.length-temp-1];
+//        System.arraycopy(array, temp+1, newArray, 0, array.length-temp-1);
+//        return newArray;
+//    }
+//
+//    public static boolean checkArray(int[] array){
+//        boolean flag1=false;
+//        boolean flag2=false;
+//        for(int i=0;i<array.length;i++){
+//            if(array[i]==4){
+//                flag1=true;
+//            }
+//            if(array[i]==1){
+//                flag2=true;
+//            }
+//        }
+//        if (flag1 && flag2){
+//            return true;
+//        }
+//        return false;
+//    }
+}
---
+++ src/test/java/ru/homework/ArrayUtilsTest.java
@@ -0,0 +1,28 @@
+package ru.homework;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ArrayUtilsTest {
+//
+//    @Test
+//    void shouldCheckCreateNewArraySuccessfully(){
+//        Assertions.assertArrayEquals(new int[]{1, 2}, ArrayUtils.changeArray(new int[]{3,2,1,4,1,2}));
+//        Assertions.assertThrows(RuntimeException.class, () -> {
+//            ArrayUtils.changeArray(new int[]{});
+//        });
+//        Assertions.assertThrows(RuntimeException.class, () -> {
+//            ArrayUtils.changeArray(new int[]{3,2,1,1,2});
+//        });
+//        Assertions.assertArrayEquals(new int[]{1,2,5,78,96}, ArrayUtils.changeArray(new int[]{4,3,2,1,4,1,2,5,78,96}));
+//    }
+//
+//    @Test
+//    void shouldCheckArrayForNumbersOneAndFourSuccessfully(){
+//        Assertions.assertTrue(ArrayUtils.checkArray(new int[]{3,2,1,4,1,2}));
+//        Assertions.assertFalse(ArrayUtils.checkArray(new int[]{3,2,4,2}));
+//        Assertions.assertFalse(ArrayUtils.checkArray(new int[]{3,2,1,1,2}));
+//        Assertions.assertFalse(ArrayUtils.checkArray(new int[]{3,2,2}));
+//    }
+
+
+}
